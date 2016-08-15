7a10140162e58a722e777066ca84c571122a4d89
Rick Wang <wangziwe@amazon.com>
2016-08-15 16:40:17 -0700

    Add CrimeFragment and wire it in CrimeActivity

--- app/src/main/java/com/mysticwind/android/bignerdranch/training/mycrimeintent/activity/CrimeActivity.java
+++ app/src/main/java/com/mysticwind/android/bignerdranch/training/mycrimeintent/activity/CrimeActivity.java
@@ -1,9 +1,12 @@
 package com.mysticwind.android.bignerdranch.training.mycrimeintent.activity;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
 import com.mysticwind.android.bignerdranch.training.mycrimeintent.R;
+import com.mysticwind.android.bignerdranch.training.mycrimeintent.activity.fragment.CrimeFragment;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -11,5 +14,15 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
