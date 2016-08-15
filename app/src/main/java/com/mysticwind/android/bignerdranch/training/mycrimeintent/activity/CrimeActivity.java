b16324322c9324251c0ea2eee9681116f89f9ed0
Rick Wang <wangziwe@amazon.com>
2016-08-15 16:19:47 -0700

    Move CrimeActivity to activity package

--- app/src/main/java/com/mysticwind/android/bignerdranch/training/mycrimeintent/activity/CrimeActivity.java
+++ app/src/main/java/com/mysticwind/android/bignerdranch/training/mycrimeintent/activity/CrimeActivity.java
@@ -1,7 +1,9 @@
-package com.mysticwind.android.bignerdranch.training.mycrimeintent;
+package com.mysticwind.android.bignerdranch.training.mycrimeintent.activity;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+
+import com.mysticwind.android.bignerdranch.training.mycrimeintent.R;
 
 public class CrimeActivity extends FragmentActivity {
 
