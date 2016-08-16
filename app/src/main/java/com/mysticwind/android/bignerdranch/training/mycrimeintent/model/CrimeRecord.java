5dac5d962b9462655061fff0a4c0347fc160df4f
Rick Wang <wangziwe@amazon.com>
2016-08-16 09:52:18 -0700

    Update CrimeRecord model

--- app/src/main/java/com/mysticwind/android/bignerdranch/training/mycrimeintent/model/CrimeRecord.java
+++ app/src/main/java/com/mysticwind/android/bignerdranch/training/mycrimeintent/model/CrimeRecord.java
@@ -1,15 +1,21 @@
 package com.mysticwind.android.bignerdranch.training.mycrimeintent.model;
 
+import org.threeten.bp.LocalDateTime;
+
 import java.util.UUID;
 
 public class CrimeRecord {
 
     private final UUID id;
-    private final String title;
+    private String title;
+    private final LocalDateTime dateTime;
+    private boolean solved;
 
     public CrimeRecord(String title) {
         this.id = UUID.randomUUID();
         this.title = title;
+        this.dateTime = LocalDateTime.now();
+        this.solved = false;
     }
 
     public UUID getId() {
@@ -18,5 +24,21 @@ public class CrimeRecord {
 
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public boolean isSolved() {
+        return solved;
+    }
+
+    public void setSolved(boolean solved) {
+        this.solved = solved;
     }
 }
