3fc946bced3c97f7142bbfb794851a15ee6580e7
Rick Wang <wangziwe@amazon.com>
2016-08-17 15:00:02 -0700

    CrimeListActivity: show two panes for tablets

--- app/src/main/java/com/mysticwind/android/bignerdranch/training/mycrimeintent/activity/CrimeListActivity.java
+++ app/src/main/java/com/mysticwind/android/bignerdranch/training/mycrimeintent/activity/CrimeListActivity.java
@@ -2,9 +2,15 @@ package com.mysticwind.android.bignerdranch.training.mycrimeintent.activity;
 
 import android.support.v4.app.Fragment;
 
+import com.mysticwind.android.bignerdranch.training.mycrimeintent.R;
 import com.mysticwind.android.bignerdranch.training.mycrimeintent.activity.fragment.CrimeListFragment;
 
 public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_masterdetail;
+    }
 
     @Override
     protected Fragment createFragment() {
--- app/src/main/java/com/mysticwind/android/bignerdranch/training/mycrimeintent/activity/SingleFragmentActivity.java
+++ app/src/main/java/com/mysticwind/android/bignerdranch/training/mycrimeintent/activity/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package com.mysticwind.android.bignerdranch.training.mycrimeintent.activity;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -11,10 +12,16 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
+
+    @LayoutRes
+    protected int getLayoutResourceId() {
+        return R.layout.empty_frame;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.empty_frame);
+        setContentView(getLayoutResourceId());
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
